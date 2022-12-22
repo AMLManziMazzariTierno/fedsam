2be4ed6e9f8d2d058879bd7d19bef85b9a86c7d6
Mazzarimovic22 <mattiamazzari@gmail.com>
2022-12-22 11:10:45 +0100

    comment out not interesting experiments

--- data/setup_datasets.sh
+++ data/setup_datasets.sh
@@ -2,16 +2,16 @@
 #
 # script to preprocess data
 
-echo "Downloading CIFAR10..."
-cd ./cifar10/preprocessing
-wget https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz
-tar -xvf cifar-10-python.tar.gz
-rm cifar-10-python.tar.gz
+# echo "Downloading CIFAR10..."
+# cd ./cifar10/preprocessing
+# wget https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz
+# tar -xvf cifar-10-python.tar.gz
+# rm cifar-10-python.tar.gz
 
-echo "Extracting images for pickle files..."
-mkdir ../data/raw
-mkdir ../data/raw/img
-python save_images.py
+# echo "Extracting images for pickle files..."
+# mkdir ../data/raw
+# mkdir ../data/raw/img
+# python save_images.py
 
 echo "Downloading CIFAR100..."
 cd ../../cifar100/preprocessing
