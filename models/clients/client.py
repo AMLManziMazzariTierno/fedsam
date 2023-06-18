39d511010a185a518d2543130c688f1519ef248a
Mazzarimovic22 <mattiamazzari@gmail.com>
2023-06-18 11:39:49 +0200

    feat: alfa 0.5, 8 clients, 1000 round

--- models/clients/client.py
+++ models/clients/client.py
@@ -180,12 +180,15 @@ class Client:
 
         for i in self._classes:
             class_label = i  # Current class label
+            print("Class label:", class_label)
             for j, data in enumerate(self.trainloader):
                 input_data_tensor, target_data_tensor = data[0].to(self.device), data[1].to(self.device)
 
                 # Filter data based on the current class label
                 mask = target_data_tensor == class_label
                 filtered_input_data = input_data_tensor[mask]
+                print("Filtered data:", filtered_input_data.shape)
+                print("Filtered data len:", len(filtered_input_data))
 
                 # Process filtered data through the model
                 if len(filtered_input_data) > 0:
